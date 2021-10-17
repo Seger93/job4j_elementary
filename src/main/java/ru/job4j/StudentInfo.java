50f028d8ce5d4651f9b6d0ecab658ec9be916278
Admin <Serger9393@mail.ru>
2021-10-17 17:57:34 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Sergei Gerbov");
+        System.out.println("13.07.93");
+
+    }
+}
