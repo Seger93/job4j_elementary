2cb2f7fd3a73bbadd157d048dea8e143c2b4957d
Admin <Serger9393@mail.ru>
2021-11-06 19:12:31 +0300

    4.3.4. if c return [#333785 #224501]

--- src/main/java/ru/job4j/condition/IfElseReturn.java
+++ src/main/java/ru/job4j/condition/IfElseReturn.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class IfElseReturn {
     public static boolean eq(int x1, int y1, int x2, int y2) {
-        return x1 == y1 || x2 == y2 || x1 == y2 || y1 == x2 || x1 == x2;
+        return x1 == x2 && y1 == y2;
     }
 
     public static void main(String[] args) {
